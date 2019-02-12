798891bc674418a797ab350fd4dd5917bd1d3240
Muhammad Aftab Aslam <formanite.2000@gmail.com>
2019-02-12 21:12:35 +0400

    Updates

---
+++ precache-manifest.975f94461f8d387bba19b179825a0ebd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b126d8b7623a1caf671b",
+    "url": "/algorythma-task/static/js/runtime~main.b126d8b7.js"
+  },
+  {
+    "revision": "5c044d4f65b822b1545c",
+    "url": "/algorythma-task/static/js/main.5c044d4f.chunk.js"
+  },
+  {
+    "revision": "759faf6a14ca8c4f5170",
+    "url": "/algorythma-task/static/js/1.759faf6a.chunk.js"
+  },
+  {
+    "revision": "5c044d4f65b822b1545c",
+    "url": "/algorythma-task/static/css/main.ae252e3e.chunk.css"
+  },
+  {
+    "revision": "ccb2ba95624b6c5d1c917b64acb8a951",
+    "url": "/algorythma-task/index.html"
+  }
+];
